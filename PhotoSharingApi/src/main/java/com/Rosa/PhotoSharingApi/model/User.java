12201156b3acfc634a1a7319ed37609902147e29
rosa <rosa@rosa-pc>
2021-01-05 13:27:47 +0330

    Model Relation

---
+++ PhotoSharingApi/src/main/java/com/Rosa/PhotoSharingApi/model/User.java
@@ -0,0 +1,115 @@
+package com.Rosa.PhotoSharingApi.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	private String name;
+	
+	
+	private String userName;
+	private String password;
+	private String email;
+	private String bio;
+	private Date createdDate;
+	private Set<UserRole> userRoles=new HashSet<>();
+	private List<Post> post;
+	private List<Post> LikedPost;
+	
+	
+	public User(int id, String name, String userName, String password, String email, String bio, Date createdDate,
+			Set<UserRole> userRoles, List<Post> post, List<Post> likedPost) {
+		
+		this.id = id;
+		this.name = name;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+		this.bio = bio;
+		this.createdDate = createdDate;
+		this.userRoles = userRoles;
+		this.post = post;
+		LikedPost = likedPost;
+	}
+	
+	public User() {
+				
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBio() {
+		return bio;
+	}
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+	public List<Post> getPost() {
+		return post;
+	}
+	public void setPost(List<Post> post) {
+		this.post = post;
+	}
+	public List<Post> getLikedPost() {
+		return LikedPost;
+	}
+	public void setLikedPost(List<Post> likedPost) {
+		LikedPost = likedPost;
+	}
+	
+	
+	
+}
---
+++ PhotoSharingApi/src/test/java/com/Rosa/PhotoSharingApi/PhotoSharingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rosa.PhotoSharingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoSharingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
