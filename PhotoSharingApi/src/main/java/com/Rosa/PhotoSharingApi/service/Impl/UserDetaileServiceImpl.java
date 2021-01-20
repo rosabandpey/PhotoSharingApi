2fd77c26c05c5486b0293deee161e1ac804af557
rosabandpey <mahnazbandpey@gmail.com>
2021-01-20 09:21:10 +0330

    Implementing security based on jdbc

---
+++ PhotoSharingApi/src/main/java/com/Rosa/PhotoSharingApi/service/Impl/UserDetaileServiceImpl.java
@@ -0,0 +1,55 @@
+package com.Rosa.PhotoSharingApi.service.Impl;
+
+
+
+import java.util.HashSet;
+
+import javax.transaction.Transactional;
+
+import  java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+import com.Rosa.PhotoSharingApi.model.User;
+import com.Rosa.PhotoSharingApi.model.UserRole;
+import com.Rosa.PhotoSharingApi.repository.AccountRepository;
+
+
+public class UserDetaileServiceImpl implements UserDetailsService {
+
+	
+	
+	
+	
+	@Autowired
+	private AccountRepository accountRepo;
+	
+	@Transactional
+	@Override
+	public UserDetails loadUserByUsername(String username)  
+				throws UsernameNotFoundException {
+		User user = accountRepo.findByUsername(username);
+		
+		if(user==null) {
+		throw new UsernameNotFoundException(
+				"User '" + username + "' not found");
+		}
+		
+		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+		Set<UserRole> userRoles = user.getUserRoles();
+	    for (UserRole  role : userRoles) {
+	        grantedAuthorities.add(new SimpleGrantedAuthority(role.getRole().toString()));
+	    }
+	    return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+	    
+		
+		}
+		
+	}
+
+
